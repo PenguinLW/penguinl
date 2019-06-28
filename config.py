c54ba34066b33617de5755a8cb1eb544e5129161
PenguinL <diana1997525@gmail.com>
2019-06-28 17:35:20 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
-
+p_token = p_base_url = p_admin_id = "";
 class P_Bot:
-    p_token = "";
-    p_base_url = "";
+    global p_token, p_base_url, p_admin_id;
     def __init__(app):
-        p_token = "";
-        p_base_url = "";
+        p_admin_id = "696236779";
+        p_token = "639880775:AAFdOtEP2m_1p5ctsB_AAUgE-zb8KSKCUKg";
+        p_base_url = "https://telegg.ru/orig/bot";
+    def get_admin_id(app):
+        return p_admin_id;
     def get_token(app):
-        return "639880775:AAFdOtEP2m_1p5ctsB_AAUgE-zb8KSKCUKg";
+        return p_token;
     def get_base_url(app):
-        return "https://telegg.ru/orig/bot";
+        return p_base_url;
