261fb91cda9b2d3de641dc0361d7c9676408b3b1
PenguinL <diana1997525@gmail.com>
2019-06-28 17:35:45 +0800

    Update penguinl.py

--- penguinl.py
+++ penguinl.py
@@ -8,7 +8,7 @@ from telegram.ext import Filters;
 
 from config import P_Bot;
 
-p_inf = P_Bot();
+command_handler = p_bot = p_inf = updater = null;
 def hola_user(p_bot: Bot, update: Update):
     p_bot.send_message(
         chat_id=update.message.chat_id,
@@ -19,28 +19,31 @@ def answer_user(p_bot: Bot, update: Update):
         chat_id=update.message.chat_id,
         text="answer"
     );
+class App():
+    global command_handler, p_bot, p_inf, updater;
+    def __init__(app):
+        commande_handler = [];
+        p_bot = Bot(
+            token=p_inf.get_token(),
+            base_url=p_inf.get_base_url()
+        );
+        p_inf = P_Bot();
+        updater = Updater(bot=p_bot);
 
-def app_run():
-    commande_handler = [];
-    p_bot = Bot(
-        token=p_inf.get_token(),
-        base_url=p_inf.get_base_url()
-    );
-    print(p_inf.get_token(), p_inf.get_base_url());
-    updater = Updater(bot=p_bot);
-    
-    commande_handler.append(CommandHandler("start", hola_user));
-    commande_handler.append(MessageHandler(Filters.text, answer_user));
-    
-    for el in commande_handler:
-        updater.dispatcher.add_handler(el);
-    while(True):
+        commande_handler.append(CommandHandler("start", hola_user));
+        commande_handler.append(MessageHandler(Filters.text, answer_user));
+        
+        for el in commande_handler:
+            updater.dispatcher.add_handler(el);
+    def app_run(app):
         p_bot.send_message(
-            chat_id=696236779,
+            chat_id=p_inf.get_admin(),
             text="worked."
         );
         updater.start_polling();
-        sleep(600);#11400);
 
 if(__name__ == "__main__"):
-    app_run();
+    app = App();
+    while(True):
+        app.app_run();
+        sleep(120);#600);#11400);
