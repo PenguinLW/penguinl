74a1926264a050d91d1e07fd59a6020d6bdd3a3e
Kovalsky95 <diana1997525@gmail.com>
2021-06-08 15:01:09 +0800

    PenguinL

---
+++ release.sh
@@ -0,0 +1,11 @@
+#git clone -o penguinl https://github.com/Kovalsky95/penguinl.git
+#git config --global user.email "diana1997525@gmail.com"
+#git config --global user.name "Kovalsky95"
+#git config --global credential.helper cache
+git config --global credential.helper "cache --timeout=3600"
+#git config credential.helper store
+#git config --unset credential.helper
+git pull --all
+git add .
+git commit -m PenguinL
+git push --set-upstream penguinl master
