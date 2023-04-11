a2813064b4838542b242e9b6cfd576e7a776f9c2
PenguinLW <diana1997525@gmail.com>
2023-04-11 07:32:13 +0000

    PenguinL

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ pip freeze > req.txt
 chmod u+x *.sh *.py
 #git clone -o penguinl https://github.com/PenguinLW/penguinl.git
 #git config --global user.email "diana1997525@gmail.com"
-#git config --global user.name "Kovalsky95"
+#git config --global user.name "PenguinLW"
 #git config --global credential.helper cache
 git config --global credential.helper "cache --timeout=25200"
 #git config credential.helper store
