52dbbdd2f87b9b4ea8d960d1deddf94ae9d72c4c
PenguinLW <diana1997525@gmail.com>
2023-07-04 18:46:59 +0800

    PenguinL

--- release.sh
+++ release.sh
@@ -3,6 +3,8 @@
 #pip install -r req.txt
 #pip freeze > req.txt
 chmod u+x *.sh *.py
+
+#git config --global --add safe.directory /media/penguinl/PenguinL/PycharmProjects/penguinl
 #git clone -o penguinl https://github.com/PenguinLW/penguinl.git
 #git config --global user.email "diana1997525@gmail.com"
 #git config --global user.name "PenguinLW"
@@ -10,6 +12,7 @@ chmod u+x *.sh *.py
 git config --global credential.helper "cache --timeout=25200"
 #git config credential.helper store
 #git config --unset credential.helper
+
 git pull --all
 git add .
 git commit -m PenguinL
