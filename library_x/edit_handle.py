7a962f80d8e093cb05685322d542c014fef7dd81
PenguinLW <diana1997525@gmail.com>
2024-10-19 13:17:59 +0800

    PenguinL

--- library_x/edit_handle.py
+++ library_x/edit_handle.py
@@ -18,8 +18,34 @@ async def la_calculadora(self):
     pass
 async def cr_unplan(self):
     pass
-async def el_minutero(self):
-    pass
+async def el_minutero(app_peng, message: types.Message):
+    tmp = message.text.replace("/el_minutero ", "").split("\n");
+    await app_peng.bot.send_message(chat_id=message.chat.id, text=f'Анализирую полученные данные ..')
+
+    await asyncio.sleep(2)
+    app_peng.p_inf.estab_unplan(chat_id=message.chat_id, tmp);
+
+    await app_peng.bot.edit_message_text(
+        chat_id=message.chat.id,
+        message_id=message.message_id + 1,
+        text='{:s} ..'.format(
+            app_peng.p_inf.get_from(message.chat.id, tmp)
+        )
+    )
+
+    # context.bot.delete_message(update.message.chat_id, update.message.message_id);
+    await asyncio.sleep(25)
+    await app_peng.bot.delete_message(chat_id=message.chat.id, message_id=message.message_id);
+
+
+    await app_peng.bot.edit_message_text(
+        chat_id=message.chat.id,
+        message_id=message.message_id + 1,
+        text=f'Удаляю ваши данные ..'
+    )
+
+    await asyncio.sleep(11)
+    await app_peng.bot.delete_message(chat_id=message.chat.id, message_id=message.message_id + 1);
 async def show_all_in(app_peng, message: types.Message):
     tmp = message.text.replace("/show_all_in ", "");
 
